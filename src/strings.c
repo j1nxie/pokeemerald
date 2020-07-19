7e7c0578d60251cfed007b06feaabe8e46babccb
j1nxie <rylieeeeexd@gmail.com>
2020-07-20 00:35:12 +0700

    change pokemon nickname in menu

--- src/strings.c
+++ src/strings.c
@@ -1817,3 +1817,4 @@ const u8 gText_Answer[] = _("Answer");
 const u8 gText_PokeBalls[] = _("Poké Balls");
 const u8 gText_Berry[] = _("Berry");
 const u8 gText_Berries[] = _("Berries");
+const u8 gText_Nickname[] = _("Nickname");
